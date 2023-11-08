d42fe7091fcfe3f90ac400c6ee7b4541e59dfd29
KKatiaa <katyakravchuk123@gmail.com>
2023-11-08 14:46:04 +0200

    add changes

--- src/index.js
+++ src/index.js
@@ -5,32 +5,45 @@ import reportWebVitals from './reportWebVitals';
 
 
 class Board extends React.Component {
-  renderSquare(i) {
-    return <Square value={i} />;
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
   }
-}
 
-class Square extends React.Component {
-  renderSquare(i) {
-    return <Board/>;
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
   }
+
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
+  }
+
   render() {
-    const status = "Next player X:";
+    const status = 'Next player: X';
 
     return (
       <div>
-        <div className='Status'>{status}</div>
-        <div className="BoardRow">
+        <div className="status">{status}</div>
+        <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
           {this.renderSquare(2)}
         </div>
-        <div className="BoardRow">
+        <div className="board-row">
           {this.renderSquare(3)}
           {this.renderSquare(4)}
           {this.renderSquare(5)}
         </div>
-        <div className="BoardRow">
+        <div className="board-row">
           {this.renderSquare(6)}
           {this.renderSquare(7)}
           {this.renderSquare(8)}
@@ -39,6 +52,27 @@ class Square extends React.Component {
     );
   }
 }
+
+class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+
+  render() {
+    return (
+      <button
+        className="square"
+        onClick={() => this.setState({value: 'X'})}
+      >
+        {this.state.value}
+      </button>
+    );
+  }
+}
+
 class Game extends React.Component {
   render() {
     return (
